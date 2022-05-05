6a2da16a24ece6678e13c187bac29d1469bd3b7a
Tanawy <amreltanawy@hotdesk-app.com>
2022-05-05 23:07:14 +0200

    add inprogress user profile page display basic data

--- src/Pages/UserProfile.js
+++ src/Pages/UserProfile.js
@@ -2,32 +2,80 @@ import React from 'react';
 import {
     EuiPageTemplate,
     EuiButton,
-    EuiFlexGrid,
     EuiFlexItem,
     EuiPanel,
-    EuiFlexGroup
+    EuiFlexGroup,
+    EuiText,
+    EuiImage,
+    EuiTitle
 } from '@elastic/eui';
-import {useAuth0} from "@auth0/auth0-react";
+import { getUser} from '../Services/AuthService';
+import UserInfoForm from "../Component/UserInfoForm";
 
 
-export default ({ button = <></> }) => (
+export default ({ button = <></> }) => {
+    let user = getUser();
 
-    <EuiPageTemplate
-        restrictWidth={false}
-        template="empty"
-        pageHeader={{
-            pageTitle: 'Profile',
-            rightSideItems: [button, <EuiButton>edit</EuiButton>],
-        }}
-    >
-        <EuiFlexGroup >
-            <EuiFlexItem grow={false}>
-                <EuiPanel style={{ height: 200 }} />
-            </EuiFlexItem>
-            <EuiFlexItem>
-                <EuiPanel style={{ height: 200 }} />
-            </EuiFlexItem>
+    let userChosenName = (user.user_metadata && user.user_metadata.name)||user.name;
+    return (
 
-        </EuiFlexGroup>
-    </EuiPageTemplate>
-);
+        <EuiPageTemplate
+            restrictWidth={false}
+            template="empty"
+            pageHeader={{
+                pageTitle: 'Profile',
+                rightSideItems: [button, <UserInfoForm />],
+            }}
+        >
+            <EuiFlexGroup style={{wrap:'nowrap'}} >
+                <EuiFlexItem grow={false}>
+                    <EuiPanel style={{height: 200, width: 'fit-content'}}>
+                        <EuiFlexGroup>
+                            <EuiFlexItem >
+                            <EuiImage
+                                style={{borderRadius:"50%"}}
+                                size={100}
+                                alt="profile picture"
+                                src={user.picture}
+                            />
+                            </EuiFlexItem>
+                        </EuiFlexGroup>
+                        <EuiText grow={false}>
+                            <h3>{user.nickname}</h3>
+                        </EuiText>
+                    </EuiPanel>
+                </EuiFlexItem>
+                <EuiFlexItem>
+                    <EuiPanel style={{height: 200, width:'fit-content'}}>
+                        <EuiFlexGroup columns={2}>
+                            <EuiFlexItem>
+                                <EuiTitle size='s'>
+                                    <h2>Name</h2>
+                                </EuiTitle>
+                                <EuiFlexItem grow={false}>
+                                    <EuiTitle size='s'>
+                                        <h2>{userChosenName}</h2>
+                                    </EuiTitle>
+                                </EuiFlexItem>
+                            </EuiFlexItem>
+
+                            <EuiFlexItem>
+                                <EuiTitle size="s">
+                                    <h2>Email: </h2>
+                                </EuiTitle>
+                                <EuiFlexItem grow={false}>
+                                    <EuiTitle size='s'>
+                                        <h2>{user.email}</h2>
+                                    </EuiTitle>
+                                </EuiFlexItem>
+                            </EuiFlexItem>
+                            <EuiFlexItem></EuiFlexItem>
+
+                        </EuiFlexGroup>
+                    </EuiPanel>
+                </EuiFlexItem>
+
+            </EuiFlexGroup>
+        </EuiPageTemplate>
+    );
+}
